fa43bc0628a1f1e8998b65cf9784c4e09c2028e3
lxz <liuxiongzhuo@163.com>
2026-02-14 10:49:09 +0800

    删除了没有必要的地方

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -6,16 +6,5 @@ App<IAppOption>({
   onLaunch() {
     authStore.hydrate()
     appStore.reset()
-
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    wx.login({
-      success: res => {
-        if (!res.code) return
-        // TODO: send code to backend for session exchange.
-      },
-    })
   },
 })
